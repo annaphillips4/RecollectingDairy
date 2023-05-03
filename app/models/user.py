e30cddc8f7f6b52866e2254178f4f3c55c8b545f
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-05-02 20:21:00 -0400

    added first and last name columns to user model

--- app/models/user.py
+++ app/models/user.py
@@ -10,9 +10,12 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+
 
     @property
     def password(self):
@@ -29,5 +32,7 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'firstName': self.first_name,
+            'lastName': self.last_name
         }
