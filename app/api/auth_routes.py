b750e99ca10d0a8e46df0cd60ca323d5e9368695
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-05-09 18:11:50 -0400

    added missing commas

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -65,8 +65,8 @@ def sign_up():
         user = User(
             username=form.data['username'],
             email=form.data['email'],
-            password=form.data['password']
-            first_name=form.data['first_name']
+            password=form.data['password'],
+            first_name=form.data['first_name'],
             last_name=form.data['last_name']
         )
         db.session.add(user)
