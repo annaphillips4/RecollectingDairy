b6da891822352e087a170051a6139d808e4a7400
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-05-10 18:04:08 -0400

    updates to list routes

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import current_user, login_required
+from sqlalchemy.orm import joinedload
 from app.models import List, db
 from app.forms import ListForm
 from .auth_routes import validation_errors_to_error_messages
@@ -29,3 +30,10 @@ def add_list():
         db.session.commit()
         return list.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+@list_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_list(id):
+   list = List.query.filter(List.id == id, List.owner_id == current_user.id).first()
+   if not list:
+      return jsonify({'error': 'List not found'}), 404
