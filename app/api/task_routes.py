675d307ea53906fca263f2f4e5fbc2e563b93542
Anna <annaphillips416@gmail.com>
2023-05-10 18:21:17 -0400

    SmartAdd parsing string; need to submit to backend

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -5,33 +5,34 @@ from app.models import Task
 task_routes = Blueprint('tasks', __name__)
 
 @task_routes.route('/')
-@login_required
+# @login_required
 def all_tasks():
     tasks = Task.query.all()
     return {task.id: task.to_dict() for task in Task.query.all()}
 
 @task_routes.route('/<int:id>')
-@login_required
+# @login_required
 def task_by_id(id):
     task = Task.query.get(id)
     return task.to_dict()
 
 @task_routes.route('/new_task', methods=['POST'])
+# @login_required
 def post_new_task():
     form = NewTask()
     if form.validate_on_submit():
         new_task = NewTask(
-            name=form.data['name']
-            completed=form.data['completed']
-            dueDate=form.data['dueDate']
-            startDate=form.data['startDate']
-            priority=form.data['priority']
-            repeat_period=form.data['repeat_period']
-            repeat_type=form.data['repeat_type']
-            location=form.data['location']
-            estimate=form.data['estimate']
-            tags=form.data['tags']
-            notes=form.data['notes']
+            name=form.data['name'],
+            completed=form.data['completed'],
+            dueDate=form.data['dueDate'],
+            startDate=form.data['startDate'],
+            priority=form.data['priority'],
+            repeat_period=form.data['repeat_period'],
+            repeat_type=form.data['repeat_type'],
+            location=form.data['location'],
+            estimate=form.data['estimate'],
+            tags=form.data['tags'],
+            notes=form.data['notes'],
             submit=form.data['submit']
         )
         db.session.add(new_task)
@@ -40,7 +41,7 @@ def post_new_task():
     return jsonify({'error': f'Unable to create new task.'})
 
 @task_routes.route('/<int:id>', methods=['DELETE'])
-@login_required
+# @login_required
 def delete_task_by_id(id):
     task = Task.query.get(id)
     if task:
--- react-app/src/components/Tasks/Tasks.js
+++ react-app/src/components/Tasks/Tasks.js
@@ -1,19 +1,80 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadTasks } from "../../store/task";
 
 export default function Tasks() {
-    const dispatch = useDispatch()
-    const tasks = useSelector(state => state.tasks)
-    const tasksArr = Object.values(tasks)
-    console.log(tasks)
+  const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.tasks);
+  const tasksArr = Object.values(tasks);
+  const [inputValue, setInputValue] = useState("");
 
-    useEffect(() => {
-        dispatch(loadTasks())
-    }, [dispatch])
-    return (
-        <>
-            {tasksArr.map(obj => <div>{obj.task}</div>)}
-        </>
-    )
+  useEffect(() => {
+    dispatch(loadTasks());
+  }, [dispatch]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formValues = parseInputString(inputValue);
+    console.log(formValues); // TODO: Submit the form values
+  }
+
+  function parseInputString(input) {
+    const tagPattern = /([!@^~#*=+])([^!@^~#*=+]+)/g;
+    const matches = input.matchAll(tagPattern);
+    const formValues = {};
+
+    for (const match of matches) {
+      const tag = match[1];
+      const value = match[2].trim();
+
+      switch (tag) {
+        case "!":
+          formValues.priority = value;
+          break;
+        case "@":
+          formValues.location = value;
+          break;
+        case "#":
+          formValues.list = value;
+          break;
+        case "*":
+          formValues.repeat = value;
+          break;
+        case "=":
+          formValues.estimate = value;
+          break;
+        case "+":
+          formValues.assignedUser = value;
+          break;
+        case "^":
+          formValues.dueDate = value;
+          break;
+        case "~":
+          formValues.startDate = value;
+          break;
+        default:
+          formValues.name = value;
+          break;
+      }
+    }
+
+    return formValues;
+  }
+
+  return (
+    <>
+      <h1>Tasks</h1>
+      {tasksArr.map((taskObj) => (
+        <div>{taskObj.name}</div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
