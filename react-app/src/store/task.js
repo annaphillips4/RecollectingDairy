efad9e954df817d76c2bf63f5c76cc4b504a7599
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-05-12 15:08:23 -0400

    task and list put actions added to store

--- react-app/src/store/task.js
+++ react-app/src/store/task.js
@@ -1,21 +1,27 @@
-const LOAD_TASKS = 'task/LOAD_TASKS'
-const ADD_TASK = 'task/ADD_TASK'
-const DELETE_TASK = 'task/DELETE_TASK'
+const LOAD_TASKS = "tasks/LOAD_TASKS"
+const ADD_TASK = "tasks/ADD_TASK"
+const UPDATE_TASK = "tasks/UPDATE_TASK"
+const DELETE_TASK = "tasks/DELETE_TASK"
 
-const load = tasks => ({
+const load = (tasks) => ({
     type: LOAD_TASKS,
     tasks
-})
+});
 
-const addTask = task => ({
+const addTask = (task) => ({
     type: ADD_TASK,
     task
-})
+});
+
+const updateTask = (task) => ({
+    type: UPDATE_TASK,
+    task
+});
 
 const removeTask = (taskId) => ({
     type: DELETE_TASK,
     taskId
-})
+});
 
 export const loadTasks = () => async dispatch => {
     console.log("in loadTasks")
@@ -24,7 +30,7 @@ export const loadTasks = () => async dispatch => {
         res = await res.json()
         dispatch(load(res))
     }
-}
+};
 
 export const postTask = (payload) => async dispatch => {
     console.log("in postTasks")
@@ -41,7 +47,40 @@ export const postTask = (payload) => async dispatch => {
     } else {
         throw res;
     }
-}
+};
+
+export const editTask = (task) => async (dispatch) => {
+    try {
+        const taskId = task.id;
+        const response = await csrfFetch(`/api/lists/${taskId}`, {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        });
+
+        if (!response.ok) {
+            const error = await response.text();
+            let errorJSON;
+            try {
+                // check to see if error is JSON
+                errorJSON = JSON.parse(error);
+            } catch {
+                // error was not from server
+                throw new Error(error);
+            }
+            throw new Error(`${errorJSON.title}: ${errorJSON.message}`);
+        }
+
+        const changedTask = await response.json();
+        dispatch(updateTask(changedTask));
+        return changedTask;
+
+    } catch (error) {
+        throw error;
+    }
+};
 
 export const deleteTask = (taskId) => async dispatch => {
     const response = await fetch(`api/tasks/${taskId}`, {
@@ -53,21 +92,23 @@ export const deleteTask = (taskId) => async dispatch => {
         dispatch(removeTask(taskId))
         return deleteMessage
     }
-}
+};
 
 const initialState = {}
 const tasksReducer = (state = initialState, action) => {
     switch(action.type){
         case LOAD_TASKS:
-            return {...state, ...action.tasks}
+            return {...state, ...action.tasks};
         case ADD_TASK:
-            return { ...state, [action.task.id]: action.task }
+            return { ...state, [action.task.id]: action.task };
+        case UPDATE_TASK:
+            return { ...state, [action.task.id]: { ...action.task } };
         case DELETE_TASK:
-            const stateMinusTask = { ...state }
-            delete stateMinusTask[action.taskId]
-            return stateMinusTask
-        default: return state
+            const stateMinusTask = { ...state };
+            delete stateMinusTask[action.taskId];
+            return stateMinusTask;
+        default: return state;
     }
-}
+};
 
-export default tasksReducer
+export default tasksReducer;
