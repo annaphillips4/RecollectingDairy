9de7b71b71b3e63b5c3bda193cc360c9c727b257
Anna <annaphillips416@gmail.com>
2023-05-13 19:35:28 -0400

    added link to all tasks

--- react-app/src/components/Lists/index.js
+++ react-app/src/components/Lists/index.js
@@ -28,6 +28,7 @@ export default function Lists() {
   return (
     <>
       <h1>Lists</h1>
+      <Link to={`/app`}>All Tasks</Link>
       {listArr.map(list => <div key={list.id} className="deletable">
         <Link to={`/app/list/${list.id}`}>{list.name}</Link>
         <i className="fa-solid fa-trash-can" onClick={() => handleDelete(list.id)}></i>
