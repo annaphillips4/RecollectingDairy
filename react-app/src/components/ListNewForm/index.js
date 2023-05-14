5ffcf8e42d6128037ef6a1b5e9837be4d64db7f0
dough <dough@Dough-PC.localdomain>
2023-05-13 21:53:21 -0500

    main page search bar

--- react-app/src/components/ListNewForm/index.js
+++ react-app/src/components/ListNewForm/index.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
 import * as listActions from "../../store/list";
 
 const NewListForm = () => {
   const dispatch = useDispatch();
-  const history = useHistory();
 
   const currentUser = useSelector(state => state.session.user)
   const [name, setName] = useState("");
