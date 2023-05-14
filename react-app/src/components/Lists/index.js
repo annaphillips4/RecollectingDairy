0f9d5fbafc764d42b4ba8c08ba31af553b9bb1fb
dough <dough@Dough-PC.localdomain>
2023-05-14 02:56:31 -0500

    sidebar

--- react-app/src/components/Lists/index.js
+++ react-app/src/components/Lists/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { loadLists, deleteList } from '../../store/list'
 import NewListForm from '../ListNewForm'
 import { Link } from 'react-router-dom';
+import "./Lists.css"
 
 
 export default function Lists() {
@@ -27,15 +28,28 @@ export default function Lists() {
 
   return (
     <>
-      <h1>Lists</h1>
-      <Link to={`/app`}>All Tasks</Link>
-      {listArr.map(list => <div key={list.id} className="deletable">
-        <Link to={`/app/list/${list.id}`}>{list.name}</Link>
-        <i className="fa-solid fa-trash-can" onClick={() => handleDelete(list.id)}></i>
-      </div>)}
+      <div className="sidebar-main-container">
+        <div className="task-container">
+          <div className="all-tasks">
+            <Link to={`/app`}>All Tasks</Link>
+          </div>
+        </div>
 
-      <div onClick={toggleFormVisibility}>Add a new list... <i className="fa-regular fa-square-plus"></i></div>
-      {isFormVisible && <NewListForm />}
+        <div className="lists-container">
+          <div className="new-list-container">
+            <h3>Lists</h3>
+            <div onClick={toggleFormVisibility}><i id="new-list-button" className="fa-regular fa-square-plus"></i></div>
+            {isFormVisible && <NewListForm />}
+          </div>
+
+          {listArr.map(list => <div key={list.id} className="list-arr" tabindex="0">
+            <a href={`/app/list/${list.id}`}>{list.name}</a>
+            <i className="fa-solid fa-trash-can" onClick={() => handleDelete(list.id)}></i>
+          </div>)}
+
+
+        </div>
+      </div>
     </>
   )
 }
