793225d0d7db36221bab56ce7edf91ce5ae675a2
Anna <annaphillips416@gmail.com>
2023-05-14 19:12:39 -0400

    list form toggles visibility after submit and fields clear

--- react-app/src/components/Lists/index.js
+++ react-app/src/components/Lists/index.js
@@ -1,14 +1,19 @@
 import { React, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadLists, deleteList } from '../../store/list'
-import NewListForm from '../ListNewForm'
 import { Link } from 'react-router-dom';
+import * as listActions from "../../store/list";
+import "./ListNewForm.css"
 import "./Lists.css"
 
 
 export default function Lists() {
   const dispatch = useDispatch()
   const lists = useSelector(state => state.lists)
+  const currentUser = useSelector(state => state.session.user)
+  const [name, setName] = useState("");
+  const [notes, setNotes] = useState("");
+  const [errors, setErrors] = useState([]);
   const [isFormVisible, setIsFormVisible] = useState(false);
 
   const listArr = Object.values(lists)
@@ -17,13 +22,30 @@ export default function Lists() {
     dispatch(loadLists())
   }, [dispatch])
 
+  const toggleFormVisibility = () => {
+    setIsFormVisible(!isFormVisible);
+    setName('')
+    setNotes('')
+    setErrors([])
+  };
+
   const handleDelete = async (listId) => {
     await dispatch(deleteList(listId));
     await dispatch(loadLists());
   }
 
-  const toggleFormVisibility = () => {
-    setIsFormVisible(!isFormVisible);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const list = { name, notes, owner_id: currentUser.id };
+
+    try {
+      await dispatch(listActions.newList(list));
+      toggleFormVisibility();
+    } catch (res) {
+      const data = await res.json();
+      if (data && data.errors) setErrors(data.errors);
+    }
   };
 
   return (
@@ -39,7 +61,29 @@ export default function Lists() {
           <div className="new-list-container">
             <h3>Lists</h3>
             <div onClick={toggleFormVisibility}><i id="new-list-button" className="fa-regular fa-square-plus"></i></div>
-            {isFormVisible && <NewListForm />}
+
+            {isFormVisible &&
+              <form onSubmit={handleSubmit}>
+                <ul className="errors">
+                  {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+
+                <div className="new-form">
+                  <input className="new-form-input"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Enter name for list" name="Name"></input>
+
+                  <textarea className="new-form-input"
+                    type="text"
+                    value={notes}
+                    onChange={(e) => setNotes(e.target.value)}
+                    placeholder="Enter notes for list" name="Notes"></textarea>
+
+                  <button className="list-button" type="submit">Create List</button>
+                </div>
+              </form>}
           </div>
 
           {listArr.map(list => <div key={list.id} className="list-arr" tabIndex="0">
