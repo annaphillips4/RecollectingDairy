21d86a5a6fb1d5ab5169441aa0bbbf8feaa96815
dough <dough@Dough-PC.localdomain>
2023-05-15 14:10:03 -0500

    FINISHED SEARCH

--- react-app/src/components/TaskInfo/index.js
+++ react-app/src/components/TaskInfo/index.js
@@ -21,23 +21,21 @@ function TaskInfo() {
   //   setQuery("");
   // }, [query]);
 
-  // const show = () => {
-  //   document.querySelector(".search-results-box").classList.remove("hidden");
-  // };
+  const show = () => {
+    document.querySelector(".search-results").classList.remove("hidden");
+  };
 
-  // const hide = (e) => {
-  //   if (!e.currentTarget.contains(e.relatedTarget)) {
-  //     document.querySelector(".search-results-box").classList.add("hidden");
-  //   };
-  // };
+  const hide = (e) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      document.querySelector(".search-results").classList.add("hidden");
+    };
+  };
 
-  // const edit = (listId, id) => {
-  //   if (document.querySelector(".search-results-box")) {
-  //     document.querySelector(".search-results-box").classList.add("hidden");
-  //     history.push(`/app/list/${listId}/${id}`);
-  //     setQuery("");
-  //   }
-  // };
+  const edit = (listId, id) => {
+    document.querySelector(".search-results").classList.add("hidden");
+    history.push(`/app/list/${listId}/${id}`);
+    setQuery("");
+  };
 
   return (
     <div className="app-container">
@@ -45,21 +43,21 @@ function TaskInfo() {
 
         <div className="search-container">
           <i id="menu-icon" className="fa-solid fa-bars"></i>
-          <div className="search-bar" /*onBlur={(e) => hide(e)}*/>
-            <input onChange={(e) => setQuery(e.target.value)} /*onFocus={() => show()}*/ className="search-input" type="text" placeholder="Search..." ></input>
+          <div className="search-bar" onBlur={(e) => hide(e)}>
+            <input onChange={(e) => setQuery(e.target.value)} onFocus={() => show()} className="search-input" type="text" placeholder="Search..." ></input>
           </div>
           <i id="search-icon" className="fa-solid fa-magnifying-glass"></i>
 
-          <div className="search-results">
+          <div className="search-results hidden">
             {taskArr.filter(task => {
               if (query === "") {
-                return tasks;
+                return null;
               } else if (task.name.toLowerCase().includes(query.toLowerCase())) {
                 return tasks;
               }
             }).map((post, idx) => (
               <div className="search-results-box" key={idx}>
-                <div className="search-card" /*onMouseDown={edit(post.listId, post.id)}*/>
+                <div className="search-card" onMouseDown={() => edit(post.listId, post.id)}>
                   <span>{post.name}</span>
                 </div>
               </div>
